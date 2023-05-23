e46a96b7f8a8bb99844107a216c7cebe3408ab2c
chandu <chandueru2002@gmail.com>
2023-05-23 23:34:14 +0530

    Create-React-app

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div >
+      <header >
+       <a href="#">Amazona </a>
+      </header>
+      <main>
+         List of products
+
+      </main>
+    </div>
+  );
+}
+
+export default App;
