57e89e9421ae35ca9e021d14d55e5b936184b09f
chandu <chandueru2002@gmail.com>
2023-05-24 16:15:17 +0530

    used reducerto ease the state changes

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,23 +1,47 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState,useReducer } from 'react';
 import data from '../data';
 import { Link } from 'react-router-dom';
 import axios from 'axios'
-function HomeScreen()
+import logger from 'use-reducer-logger'
+const reducer=(state,action)=>{
+   switch(action.type)
+   {
+      case 'FETCH_REQUEST':
+         return {...state,loading:true};
+      case 'FETCH_SUCCESS':
+          return {...state,products:action.payload,loading:false};   
+      case 'FETCH_FAIL    ':
+       return {...state,error:action.payload,loading:false};
+      default:
+         return state; 
+    }
+};
+function HomeScreen() 
 {
-   let [products,setProdroducts]=useState([]);
+   //let [products,setProdroducts]=useState([]);
+   const[{loading,error,products},dispatch]=useReducer(logger(reducer),{loading:true,error:'',products:[] });
    useEffect(()=>
    {
    const fetchData=async()=>
    {
-      let res=await axios.get("/product");
-      setProdroducts(res.data);
-   }
+      dispatch({type:'FETCH_REQUEST'});
+      try{
+         let res=await axios.get("/product");
+         dispatch({type:'FETCH_SUCCESS',payload:res.data})
+      }catch(err)
+      {
+         dispatch({type:'FETCH_FAIL',payload:err.message})
+      }
+      
+      //setProdroducts(res.data);
+   }; 
    fetchData();
    },[])
      return (
         <div>
                      <h1> List of featured products</h1>
-         <div className="products">
+                     {loading?<div> Loading ....</div>:error?<div>{error.message}</div>:
+                     <div className="products">
          {
           products.map((product)=>(<div key={product.slug} className="product">
           <Link to={`/product/${product.slug}`}><img src={product.image}></img> </Link>
@@ -32,6 +56,8 @@ function HomeScreen()
           </div>))
          }
          </div>
+      }
+        
          
          
         
